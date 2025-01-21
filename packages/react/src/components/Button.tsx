8d6a8da5a115a03374d28c8fe2a640e8d4f00e82
Leif DeJong <leifdejong@gmail.com>
2025-01-21 20:50:17 +0100

    Adding button styles

--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -6,69 +6,21 @@ import { TextClassContext } from './Text';
 import { Icon } from './Icon';
 import * as React from "react";
 import { Spinner, SpinnerProps } from './Spinner'
+//
+// border-radius: var(--layout-radius, 8px);
+// background: var(--color-neutral-9, #6E6E6E);
+//
+// /* button/elevated/box-shadow */
+// box-shadow: var(--button-elevated-box-shadow-top-1-position-x, 0px) var(--button-elevated-box-shadow-top-1-position-y, 0px) var(--button-elevated-box-shadow-top-1-blur, 0px) var(--button-elevated-box-shadow-top-1-spread, 1px) var(--button-elevated-box-shadow-top-1-color, rgba(255, 255, 255, 0.10)) inset, var(--button-elevated-box-shadow-top-2-position-x, 0px) var(--button-elevated-box-shadow-top-2-position-y, 4px) var(--button-elevated-box-shadow-top-2-blur, 2px) var(--button-elevated-box-shadow-top-2-spread, -2px) var(--button-elevated-box-shadow-top-2-color, rgba(255, 255, 255, 0.15)) inset, var(--button-elevated-box-shadow-top-3-position-x, 0px) var(--button-elevated-box-shadow-top-3-position-y, 1px) var(--button-elevated-box-shadow-top-3-blur, 1px) var(--button-elevated-box-shadow-top-3-spread, 0px) var(--button-elevated-box-shadow-top-3-color, rgba(255, 255, 255, 0.40)) inset, var(--button-elevated-box-shadow-top-4-position-x, 0px) var(--button-elevated-box-shadow-top-4-position-y, -1px) var(--button-elevated-box-shadow-top-4-blur, 1px) var(--button-elevated-box-shadow-top-4-spread, 0px) var(--button-elevated-box-shadow-top-4-color, rgba(0, 0, 0, 0.40)) inset, var(--button-elevated-box-shadow-bottom-1-position-x, 0px) var(--button-elevated-box-shadow-bottom-1-position-y, 0px) var(--button-elevated-box-shadow-bottom-1-blur, 0px) var(--button-elevated-box-shadow-bottom-1-spread, 0px) var(--button-elevated-box-shadow-bottom-1-color, rgba(255, 255, 255, 0.00)) inset, var(--button-elevated-box-shadow-bottom-2-position-x, 0px) var(--button-elevated-box-shadow-bottom-2-position-y, 0px) var(--button-elevated-box-shadow-bottom-2-blur, 0px) var(--button-elevated-box-shadow-bottom-2-spread, 0px) var(--button-elevated-box-shadow-bottom-2-color, rgba(255, 255, 255, 0.00)) inset;
 
 // group flex items-center justify-center rounded-4 web:ring-offset-background web:transition-colors web:focus-visible:outline-none web:focus-visible:ring-2 web:focus-visible:ring-ring web:focus-visible:ring-offset-2
 
 // box-shadow: var(--button-elevated-box-shadow-top-1-position-x, 0px) var(--button-elevated-box-shadow-top-1-position-y, 0px) var(--button-elevated-box-shadow-top-1-blur, 0px) var(--button-elevated-box-shadow-top-1-spread, 1px) var(--button-elevated-box-shadow-top-1-color, rgba(255, 255, 255, 0.10)) inset, var(--button-elevated-box-shadow-top-2-position-x, 0px) var(--button-elevated-box-shadow-top-2-position-y, 4px) var(--button-elevated-box-shadow-top-2-blur, 2px) var(--button-elevated-box-shadow-top-2-spread, -2px) var(--button-elevated-box-shadow-top-2-color, rgba(255, 255, 255, 0.15)) inset, var(--button-elevated-box-shadow-top-3-position-x, 0px) var(--button-elevated-box-shadow-top-3-position-y, 1px) var(--button-elevated-box-shadow-top-3-blur, 1px) var(--button-elevated-box-shadow-top-3-spread, 0px) var(--button-elevated-box-shadow-top-3-color, rgba(255, 255, 255, 0.40)) inset, var(--button-elevated-box-shadow-top-4-position-x, 0px) var(--button-elevated-box-shadow-top-4-position-y, -1px) var(--button-elevated-box-shadow-top-4-blur, 1px) var(--button-elevated-box-shadow-top-4-spread, 0px) var(--button-elevated-box-shadow-top-4-color, rgba(0, 0, 0, 0.40)) inset, var(--button-elevated-box-shadow-bottom-1-position-x, 0px) var(--button-elevated-box-shadow-bottom-1-position-y, 0px) var(--button-elevated-box-shadow-bottom-1-blur, 0px) var(--button-elevated-box-shadow-bottom-1-spread, 0px) var(--button-elevated-box-shadow-bottom-1-color, rgba(255, 255, 255, 0.00)) inset, var(--button-elevated-box-shadow-bottom-2-position-x, 0px) var(--button-elevated-box-shadow-bottom-2-position-y, 0px) var(--button-elevated-box-shadow-bottom-2-blur, 0px) var(--button-elevated-box-shadow-bottom-2-spread, 0px) var(--button-elevated-box-shadow-bottom-2-color, rgba(255, 255, 255, 0.00)) inset;
 
-const buttonVariants = cva(
-  'flex flex-row gap-1 items-center justify-center flex-shrink-0 rounded-4 border web:transition-colors',
-  {
-    variants: {
-      color: {
-        neutral: 'bg-neutral-9 border-neutral-9 hover:bg-primary-a4',
-        primary: 'bg-primary-9 border-primary-9',
-        secondary: 'bg-secondary-9 border-secondary-9',
-        error: 'bg-error-9 border-error-9',
-        warning: 'bg-warning-9 border-warning-9',
-        success: 'bg-success-9 border-success-9',
-      },
-      variant: {
-        elevated: '',
-        solid: '',
-        soft: '',
-        outline: '',
-        dashed: 'border-dashed',
-        ghost: 'border-transparent',
-
-        // legacy
-        // default: 'bg-primary web:hover:opacity-90 active:opacity-90',
-        // destructive: 'bg-destructive web:hover:opacity-90 active:opacity-90',
-        // outline:
-        //   'border border-input bg-background web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
-        // secondary: 'bg-secondary web:hover:opacity-80 active:opacity-80',
-        // ghost: 'web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
-        // link: 'web:underline-offset-4 web:hover:underline web:focus:underline '
-      },
-      size: {
-        sm: 'h-9 rounded-md px-3',
-        md: 'px-3 py-2',
-        lg: 'h-11 rounded-md px-8 native:h-14',
-        icon: 'h-10 w-10',
-      },
-    },
-    compoundVariants: [
-      { variant: 'soft', color: 'neutral', class: 'bg-neutral-a3 border-neutral-3' },
-      { variant: 'soft', color: 'primary', class: 'bg-primary-a3 border-primary-3' },
-      { variant: 'soft', color: 'secondary', class: 'bg-secondary-a3 border-secondary-3' },
-      { variant: 'soft', color: 'error', class: 'bg-error-a3 border-error-3' },
-      { variant: 'soft', color: 'warning', class: 'bg-warning-a3 border-warning-3' },
-      { variant: 'soft', color: 'success', class: 'bg-success-a3 border-success-3' },
-      { variant: ['outline', 'dashed', 'ghost'], class: 'bg-transparent' },
-      // { variant: 'ghost', class: 'border-transparent' },
-    ],
-    defaultVariants: {
-      variant: 'elevated',
-      color: 'neutral',
-      size: 'md',
-    },
-  }
-);
-
 // web:whitespace-nowrap text-sm native:text-base font-medium text-foreground web:transition-colors
 
-const buttonTextVariants = cva(
-  '',
+const buttonVariants = cva(
+  'group flex flex-row gap-1 items-center self-start justify-center rounded-4',
   {
     variants: {
       color: {
@@ -80,8 +32,104 @@ const buttonTextVariants = cva(
         success: '',
       },
       variant: {
-        elevated: 'color-white',
-        solid: 'color-white',
+        elevated: 'shadow-elevated web:enabled:hover:shadow-none',
+        solid: '',
+        soft: '',
+        outline: 'border',
+        dashed: 'border border-dashed',
+        ghost: '',
+
+        // legacy
+        // default: 'bg-primary web:hover:opacity-90 active:opacity-90',
+        // destructive: 'bg-destructive web:hover:opacity-90 active:opacity-90',
+        // outline:
+        //   'border border-input bg-background web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
+        // secondary: 'bg-secondary web:hover:opacity-80 active:opacity-80',
+        // ghost: 'web:hover:bg-accent web:hover:text-accent-foreground active:bg-accent',
+        // link: 'web:underline-offset-4 web:hover:underline web:focus:underline '
+      },
+      size: {
+        sm: 'px-2.5 py-[0.34375rem]',
+        md: 'px-3 py-2',
+        lg: 'px-3.5 py-[0.65625rem]',
+        icon: 'h-10 w-10',
+      },
+
+      disabled: {
+        false: null,
+        true: ['opacity-40 web:cursor-not-allowed'],
+      },
+    },
+    compoundVariants: [
+
+      // TBD: finish this
+      { disabled: true, color: 'neutral', class: '' },
+      { disabled: true, color: 'primary', class: '' },
+      { disabled: true, color: 'secondary', class: '' },
+      { disabled: true, color: 'error', class: '' },
+      { disabled: true, color: 'warning', class: '' },
+      { disabled: true, color: 'success', class: '' },
+      //
+      // { variant: 'elevated', color: 'neutral', class: 'bg-neutral-9 web:enabled:hover:bg-neutral-10' },
+      // { variant: 'elevated', color: 'primary', class: 'bg-primary-9 web:enabled:hover:bg-primary-10' },
+      // { variant: 'elevated', color: 'secondary', class: 'bg-secondary-9 web:enabled:hover:bg-secondary-10' },
+      // { variant: 'elevated', color: 'error', class: 'bg-error-9 web:enabled:hover:bg-error-10' },
+      // { variant: 'elevated', color: 'warning', class: 'bg-warning-9 web:enabled:hover:bg-warning-10' },
+      // { variant: 'elevated', color: 'success', class: 'bg-success-9 web:enabled:hover:bg-success-10' },
+
+      { variant: ['solid', 'elevated'], color: 'neutral', class: 'bg-neutral-9 web:enabled:hover:bg-neutral-10' },
+      { variant: ['solid', 'elevated'], color: 'primary', class: 'bg-primary-9 web:enabled:hover:bg-primary-10' },
+      { variant: ['solid', 'elevated'], color: 'secondary', class: 'bg-secondary-9 web:enabled:hover:bg-secondary-10' },
+      { variant: ['solid', 'elevated'], color: 'error', class: 'bg-error-9 web:enabled:hover:bg-error-10' },
+      { variant: ['solid', 'elevated'], color: 'warning', class: 'bg-warning-9 web:enabled:hover:bg-warning-10' },
+      { variant: ['solid', 'elevated'], color: 'success', class: 'bg-success-9 web:enabled:hover:bg-success-10' },
+
+      { variant: 'soft', color: 'neutral', class: 'bg-neutral-a3 web:enabled:hover:bg-neutral-a4' },
+      { variant: 'soft', color: 'primary', class: 'bg-primary-a3 web:enabled:hover:bg-primary-a4' },
+      { variant: 'soft', color: 'secondary', class: 'bg-secondary-a3 web:enabled:hover:bg-secondary-a4' },
+      { variant: 'soft', color: 'error', class: 'bg-error-a3 web:enabled:hover:bg-error-a4' },
+      { variant: 'soft', color: 'warning', class: 'bg-warning-a3 web:enabled:hover:bg-warning-a4' },
+      { variant: 'soft', color: 'success', class: 'bg-success-a3 web:enabled:hover:bg-success-a4' },
+
+      { variant: ['outline', 'dashed'], color: 'neutral', class: 'border-neutral-a7 web:enabled:hover:border-neutral-a8' },
+      { variant: ['outline', 'dashed'], color: 'primary', class: 'border-primary-a7 web:enabled:hover:border-primary-a8' },
+      { variant: ['outline', 'dashed'], color: 'secondary', class: 'border-secondary-a7 web:enabled:hover:border-secondary-a8' },
+      { variant: ['outline', 'dashed'], color: 'error', class: 'border-error-a7 web:enabled:hover:border-error-a8' },
+      { variant: ['outline', 'dashed'], color: 'warning', class: 'border-warning-a7 web:enabled:hover:border-warning-a8' },
+      { variant: ['outline', 'dashed'], color: 'success', class: 'border-success-a7 web:enabled:hover:border-success-a8' },
+
+      { variant: ['outline', 'dashed', 'ghost'], color: 'neutral', class: 'web:enabled:hover:bg-neutral-a3' },
+      { variant: ['outline', 'dashed', 'ghost'], color: 'primary', class: 'web:enabled:hover:bg-primary-a3' },
+      { variant: ['outline', 'dashed', 'ghost'], color: 'secondary', class: 'web:enabled:hover:bg-secondary-a3' },
+      { variant: ['outline', 'dashed', 'ghost'], color: 'error', class: 'web:enabled:hover:bg-error-a3' },
+      { variant: ['outline', 'dashed', 'ghost'], color: 'warning', class: 'web:enabled:hover:bg-warning-a3' },
+      { variant: ['outline', 'dashed', 'ghost'], color: 'success', class: 'web:enabled:hover:bg-success-a3' },
+    ],
+    defaultVariants: {
+      variant: 'elevated',
+      color: 'neutral',
+      size: 'md',
+      disabled: false,
+    },
+  }
+);
+
+
+const buttonTextVariants = cva(
+  '',
+  {
+    variants: {
+      color: {
+        neutral: 'text-neutral-a11',
+        primary: 'text-primary-a11',
+        secondary: 'text-secondary-a11',
+        error: 'text-error-a11',
+        warning: 'text-warning-a11',
+        success: 'text-success-a11',
+      },
+      variant: {
+        elevated: '',
+        solid: '',
         soft: '',
         outline: '',
         dashed: '',
@@ -94,20 +142,15 @@ const buttonTextVariants = cva(
         // link: 'text-primary group-active:underline',
       },
       size: {
-        sm: '',
-        md: '',
-        lg: '',
+        sm: 'text-sm',
+        md: 'text-base',
+        lg: 'text-lg',
         icon: '',
       },
     },
     compoundVariants: [
-      { variant: ['soft', 'outline', 'dashed', 'ghost'], color: 'neutral', class: 'color-neutral-a11' },
-      { variant: ['soft', 'outline', 'dashed', 'ghost'], color: 'primary', class: 'color-primary-a11' },
-      { variant: ['soft', 'outline', 'dashed', 'ghost'], color: 'secondary', class: 'color-secondary-a11' },
-      { variant: ['soft', 'outline', 'dashed', 'ghost'], color: 'error', class: 'color-error-a11' },
-      { variant: ['soft', 'outline', 'dashed', 'ghost'], color: 'warning', class: 'color-warning-a11' },
-      { variant: ['soft', 'outline', 'dashed', 'ghost'], color: 'success', class: 'color-success-a11' },
-      { variant: ['elevated', 'solid'], color: 'warning', class: 'color-[#21201C]' },
+      { variant: ['elevated', 'solid'], class: 'text-white' },
+      { variant: ['elevated', 'solid'], color: 'warning', class: 'text-neutral-12' },
     ],
     defaultVariants: {
       color: 'neutral',
@@ -141,22 +184,25 @@ type PressableProps = Omit<React.ComponentPropsWithoutRef<typeof Pressable>, key
 
 interface ButtonProps extends PressableProps, VariantProps<typeof buttonVariants> {
   loading?: boolean;
+  asChild?: boolean;
 }
 
+export type ButtonColors = NonNullable<ButtonProps['color']>;
+export type ButtonSizes = NonNullable<ButtonProps['size']>;
+export type ButtonVariant = NonNullable<ButtonProps['variant']>;
+
 const Button = forwardRef<ElementRef<typeof Pressable>, ButtonProps>(
-  ({ className, variant, color, loading = false, size, ...props }, ref) => {
+  ({ className, variant, color, loading = false, size, disabled, ...props }, ref) => {
     return (
       <ButtonContext.Provider value={{ variant, color, size, loading }}>
         <TextClassContext.Provider
-          value={buttonTextVariants({ variant, color, size, className: 'web:pointer-events-none' })}
+          value={buttonTextVariants({ variant, color, size })}
         >
           <Pressable
-            className={cn(
-              props.disabled && 'opacity-50 web:pointer-events-none',
-              buttonVariants({ variant, color, size, className })
-            )}
+            className={buttonVariants({ variant, color, size, className, disabled })}
             ref={ref}
             role='button'
+            disabled={disabled}
             {...props}
           />
         </TextClassContext.Provider>
@@ -189,7 +235,7 @@ const ButtonIcon = forwardRef<ElementRef<typeof Icon>, ButtonIconProps>(
     );
   }
 );
-Button.displayName = 'ButtonIcon';
+ButtonIcon.displayName = 'ButtonIcon';
 
 interface ButtonSpinnerProps extends SpinnerProps {
   loadingIcon?: ComponentType<any>;
