1e3bc116013bbdc1df7891aea63ba390d2c7e466
Leif DeJong <leifdejong@gmail.com>
2025-06-19 11:18:46 -0500

    Fixing theme switcher for expo

--- packages/ui/src/components/theme/ThemeIcon.tsx
+++ packages/ui/src/components/theme/ThemeIcon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useThemeColors } from './useThemeColors';
+import Svg, { Path, Circle } from 'react-native-svg';
 
 interface ThemeIconProps {
   size?: number;
@@ -14,8 +15,7 @@ export const ThemeIcon: React.FC<ThemeIconProps> = ({
   const colors = useThemeColors();
 
   return (
-    <svg 
-      xmlns="http://www.w3.org/2000/svg" 
+    <Svg 
       width={size} 
       height={size} 
       viewBox="0 0 24 24"
@@ -27,13 +27,13 @@ export const ThemeIcon: React.FC<ThemeIconProps> = ({
       className={className}
       {...otherProps}
     >
-      <path d="M12 22a1 1 0 0 1 0-20 10 9 0 0 1 10 9 5 5 0 0 1-5 5h-2.25a1.75 1.75 0 0 0-1.4 2.8l.3.4a1.75 1.75 0 0 1-1.4 2.8z"/>
-      <circle cx="13.5" cy="6.5" r=".8" stroke={colors.warning9} strokeWidth="1.6"/>
-      <circle cx="17.5" cy="10.5" r=".8" stroke={colors.success9} strokeWidth="1.6"/>
-      <circle cx="6.5" cy="12.5" r=".8" stroke={colors.error9} strokeWidth="1.6"/>
-      <circle cx="8.5" cy="7.5" r=".8" stroke={colors.secondary9} strokeWidth="1.6"/>
-      <circle cx="12" cy="12" r=".8" stroke={colors.neutral9} strokeWidth="1.6"/>
-      <circle cx="9" cy="17" r=".8" stroke={colors.info9} strokeWidth="1.6" />
-    </svg>
+      <Path d="M12 22a1 1 0 0 1 0-20 10 9 0 0 1 10 9 5 5 0 0 1-5 5h-2.25a1.75 1.75 0 0 0-1.4 2.8l.3.4a1.75 1.75 0 0 1-1.4 2.8z"/>
+      <Circle cx="13.5" cy="6.5" r=".8" stroke={colors.warning9} strokeWidth="1.6"/>
+      <Circle cx="17.5" cy="10.5" r=".8" stroke={colors.success9} strokeWidth="1.6"/>
+      <Circle cx="6.5" cy="12.5" r=".8" stroke={colors.error9} strokeWidth="1.6"/>
+      <Circle cx="8.5" cy="7.5" r=".8" stroke={colors.secondary9} strokeWidth="1.6"/>
+      <Circle cx="12" cy="12" r=".8" stroke={colors.neutral9} strokeWidth="1.6"/>
+      <Circle cx="9" cy="17" r=".8" stroke={colors.info9} strokeWidth="1.6" />
+    </Svg>
   );
 }; 
